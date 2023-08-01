37695b1ff2f43ba41430043374ba5219182a5f2a
Zehan Alam <mjzehan@gmail.com>
2023-08-02 00:44:16 +0600

    preloader duration changed

--- js/main.js
+++ js/main.js
@@ -40,11 +40,13 @@ function openLink(event) {
             // force page scroll position to top at page refresh
             // $('html, body').animate({ scrollTop: 0 }, 'normal');
 
-            // will first fade out the loading animation 
-            $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            }); 
+            // Show preloader for the first 1.5 seconds
+            setTimeout(function() {
+                $("#loader").fadeOut("slow", function() {
+                    // will fade out the whole DIV that covers the website.
+                    $("#preloader").delay(300).fadeOut("slow");
+                });
+            }, 1500); // Set the delay time to 1500ms (1.5 seconds)
             
             // for hero content animations 
             $("html").removeClass('ss-preload');
